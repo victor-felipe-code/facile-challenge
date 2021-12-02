a0cffd90cf365be0d0b3c11c2725d17a3267835e
victorfelipeoliveirarosa <victorfelipeoliveira1@gmail.com>
2021-12-02 19:30:33 -0300

    .

--- dist/helpers/statusMessage.js
+++ dist/helpers/statusMessage.js
@@ -11,7 +11,7 @@ exports.MESSAGE_CUSTOM = MESSAGE_CUSTOM;
   MESSAGE_CUSTOM["INVALID_ID"] = "Campo \"id\" inv\xE1lido";
   MESSAGE_CUSTOM["REQUIRED_NAME"] = "O campo \"name\" \xE9 obrigat\xF3rio";
   MESSAGE_CUSTOM["REQUIRED_ID"] = "O campo \"id\" \xE9 obrigat\xF3rio";
-  MESSAGE_CUSTOM["ID_NOT_EXISTS"] = "";
+  MESSAGE_CUSTOM["ID_NOT_EXISTS"] = "Id n\xE3o existe";
   MESSAGE_CUSTOM["DELETED_OK"] = "";
   MESSAGE_CUSTOM["SERVER_ERROR"] = "Internal server error";
 })(MESSAGE_CUSTOM || (exports.MESSAGE_CUSTOM = MESSAGE_CUSTOM = {}));
--- src/helpers/statusMessage.ts
+++ src/helpers/statusMessage.ts
@@ -2,7 +2,7 @@ export enum MESSAGE_CUSTOM {
   INVALID_ID = 'Campo "id" inválido',
   REQUIRED_NAME = 'O campo "name" é obrigatório',
   REQUIRED_ID = 'O campo "id" é obrigatório',
-  ID_NOT_EXISTS = "",
+  ID_NOT_EXISTS = "Id não existe",
   DELETED_OK = "",
   SERVER_ERROR = "Internal server error",
 }
